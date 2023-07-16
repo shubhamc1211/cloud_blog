7325e14d865ec4fd390f7190ae037bf5c9d26e70
shubhamc <sh572302@dal.ca>
2023-07-16 19:18:53 -0300

    updated var

--- src/util/APIs.js
+++ src/util/APIs.js
@@ -1,13 +1,17 @@
-import "dotenv/config";
+import {
+  REACT_APP_API_GETDATA,
+  REACT_APP_API_PUTDATA,
+  REACT_APP_API_SNS,
+  REACT_APP_API_TEXT,
+} from "./URLs";
 
-const apiGetDataUrl = process.env.REACT_APP_API_GETDATA;
-const apiPutDataUrl = process.env.REACT_APP_API_PUTDATA;
-const apiSnsUrl = process.env.REACT_APP_API_SNS;
-const apiTextUrl = process.env.REACT_APP_API_TEXT;
+const apiGetDataUrl = REACT_APP_API_GETDATA;
+const apiPutDataUrl = REACT_APP_API_PUTDATA;
+const apiSnsUrl = REACT_APP_API_SNS;
+const apiTextUrl = REACT_APP_API_TEXT;
 
 console.log("In APIs:");
-console.log(process.env);
-console.log(process.env.API_PUTDATA);
+console.log(REACT_APP_API_GETDATA);
 const APIs = {
   API_SAVE_BLOGS: apiPutDataUrl,
   API_GET_BLOG: apiGetDataUrl,
