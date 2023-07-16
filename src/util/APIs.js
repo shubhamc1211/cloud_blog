98c3c945add482432a83e2ff82a5529084ea5d1a
shubhamc <sh572302@dal.ca>
2023-07-16 18:57:21 -0300

    updated APIs

--- src/util/APIs.js
+++ src/util/APIs.js
@@ -1,13 +1,18 @@
-import "dotenv";
+import "dotenv/config";
+
+const apiGetDataUrl = process.env.REACT_APP_API_GETDATA;
+const apiPutDataUrl = process.env.REACT_APP_API_PUTDATA;
+const apiSnsUrl = process.env.REACT_APP_API_SNS;
+const apiTextUrl = process.env.REACT_APP_API_TEXT;
 
 console.log("In APIs:");
 console.log(process.env);
 console.log(process.env.API_PUTDATA);
 const APIs = {
-  API_SAVE_BLOGS: process.env.API_PUTDATA,
-  API_GET_BLOG: process.env.API_GETDATA,
-  API_TRANSLATE: process.env.API_TEXT,
-  API_SNS: process.env.API_SNS,
+  API_SAVE_BLOGS: apiPutDataUrl,
+  API_GET_BLOG: apiGetDataUrl,
+  API_TRANSLATE: apiTextUrl,
+  API_SNS: apiSnsUrl,
 };
 
 export default APIs;
